30406001d23d55fff1937c1e8f4ef76defeaf984
Somatic-pipeline <41589114+Somatic-pipeline@users.noreply.github.com>
2018-12-12 10:54:55 -0600

    Add files via upload

--- summarize_cnv.R
+++ summarize_cnv.R
@@ -23,7 +23,8 @@ if (!file.exists(args[2])) {dir.create(args[2])}
 
 chrom_size_file=list.files(args[3],pattern="chrom.sizes.txt",full.names = T)
 ch_size=read.table(chrom_size_file,stringsAsFactors = F,row.names = 1)
-ch_size=ch_size[paste("chr",1:22,sep=""),,drop=F]
+#ch_size=ch_size[paste("chr",c(1:22,"X","Y"),sep=""),,drop=F]
+ch_size=ch_size[paste("chr",c(1:22),sep=""),,drop=F]
 colnames(ch_size)="len"
 ch_size$len=ch_size$len/1.0
 ch_size$cumulative_len=ch_size$len[1]
@@ -40,6 +41,34 @@ rownames(gene_loc)=gene_loc$V1
 
 cnv_result_all=c()
 
+# plotting function
+plot_cnv<-function(title,ch_size,gene_loc,cnv_result)
+{
+  cnv_result=cnv_result[cnv_result$gene %in% gene_loc$V1,]
+  gene_loc1=gene_loc[cnv_result$gene,]
+  gene_loc1$V5=(gene_loc1$V5+ch_size[gene_loc1$V3,"cumulative_len"]-ch_size[gene_loc1$V3,"len"])/max_len
+  
+  plot(1:2,1:2,xlim=c(0,1),ylim=c(0,4),type="n",ylab="CNV",xlab="",xaxt="n",main=title)
+  abline(v=0)
+  for (i in 1:dim(ch_size)[1]) 
+  {
+    abline(v=ch_size$cumulative_len[i]/max_len)
+    x1=ch_size$cumulative_len[i]-ch_size$len[i]
+    x2=ch_size$cumulative_len[i]
+    col=c('lightgrey','gray95')[1+(round(i/2)==i/2)]
+    rect(x1/max_len,-0.12,x2/max_len,4.12,col=col,border=col)
+    text((x1+x2)/2/max_len,cex=0.7,par("usr")[3]-0.2,labels=rownames(ch_size)[i],srt=45,pos=1,xpd=TRUE)
+  }
+  
+  # plot CNVs
+  for (i in 1:dim(gene_loc1)[1])
+  {
+    segments(x0=gene_loc1$V5[i],x1=gene_loc1$V5[i],y0=2,y1=cnv_result$cnv[i],lwd=0.1,
+             c("red","blue")[1+(cnv_result$cnv[i]<2)])
+  }
+  abline(h=2)
+}
+
 pdf(paste(args[2],"/cnv_plot.pdf",sep=""),height=10,width=6)
 par(mfrow=c(5,1))
 
@@ -51,29 +80,13 @@ for (j in 1:dim(design)[1])
   cnv_result_all=cbind(cnv_result_all,cnv_result$cnv)
   rownames(cnv_result_all)=cnv_result$gene
   
-  cnv_result=cnv_result[cnv_result$gene %in% gene_loc$V1,]
-  gene_loc1=gene_loc[cnv_result$gene,]
-  gene_loc1$V5=(gene_loc1$V5+ch_size[gene_loc1$V3,"cumulative_len"]-ch_size[gene_loc1$V3,"len"])/max_len
-  
   # set up plotting area
-  plot(1:2,1:2,xlim=c(0,1),ylim=c(0,4),type="n",ylab="CNV",xlab="",xaxt="n",main=design$sample_id[j])
-  abline(v=0)
-  for (i in 1:dim(ch_size)[1]) 
-  {
-    abline(v=ch_size$cumulative_len[i]/max_len)
-    text((ch_size$cumulative_len[i]-ch_size$len[i]/2)/max_len, cex=0.7,
-         par("usr")[3] - 0.2, labels = paste("chr",i,sep=""), srt = 45, pos = 1, xpd = TRUE)
-  }
-  
-  # plot CNVs
-  for (i in 1:dim(gene_loc)[1])
-  {
-    segments(x0=gene_loc1$V5[i],x1=gene_loc1$V5[i],y0=2,y1=cnv_result$cnv[i],lwd=0.1,
-             c("red","blue")[1+(cnv_result$cnv[i]<2)])
-  }
-  abline(h=2)
+  plot_cnv(design$sample_id[j],ch_size,gene_loc,cnv_result)
 }
 
+# generate a summary plot
+cnv_result[,2]=apply(cnv_result_all,1,mean)
+plot_cnv("Summary",ch_size,gene_loc,cnv_result)
 dev.off()
 
 colnames(cnv_result_all)=design$sample_id
@@ -82,6 +95,11 @@ write.csv(cnv_result_all,file=paste(args[2],"/cnv_results.csv",sep=""))
 #########  summarize quality metrics  #######################
 
 quality=c()
+fields=c("Basic Statistics","Per base sequence quality","Per tile sequence quality",
+         "Per sequence quality scores","Per base sequence content",
+         "Per sequence GC content","Per base N content",
+         "Sequence Length Distribution","Sequence Duplication Levels","Overrepresented sequences",
+         "Adapter Content","Kmer Content" )
 
 for (j in 1:dim(design)[1])
 {
@@ -92,21 +110,27 @@ for (j in 1:dim(design)[1])
   R12=list.files(paste(design$folder[j],"/tumor/fastqc",sep=""),full=T)
   R12=R12[!grepl("html",R12)]
   R12=R12[!grepl("zip",R12)]
-  R12_quality_tumor=c(sum(read.table(paste(R12[1],"/summary.txt",sep=""),sep="\t")[,1]=="FAIL"),
-                sum(read.table(paste(R12[2],"/summary.txt",sep=""),sep="\t")[,1]=="FAIL"))
+  R12_quality_tumor=c(read.table(paste(R12[1],"/summary.txt",sep=""),sep="\t",stringsAsFactors=F)[,1],
+                      read.table(paste(R12[2],"/summary.txt",sep=""),sep="\t",stringsAsFactors=F)[,1])
+  
+  tmp=read.table(paste(R12[1],"/summary.txt",sep=""),sep="\t",stringsAsFactors=F)[,2]
+  names(R12_quality_tumor)=c(paste("T1_",tmp,sep=""),paste("T2_",tmp,sep=""))
+  R12_quality_tumor=R12_quality_tumor[c(paste("T1_",fields,sep=""),paste("T2_",fields,sep=""))]
   
   # normal quality
   R12=list.files(paste(design$folder[j],"/normal/fastqc",sep=""),full=T)
   R12=R12[!grepl("html",R12)]
   R12=R12[!grepl("zip",R12)]
-  R12_quality_normal=c(sum(read.table(paste(R12[1],"/summary.txt",sep=""),sep="\t")[,1]=="FAIL"),
-                      sum(read.table(paste(R12[2],"/summary.txt",sep=""),sep="\t")[,1]=="FAIL"))
+  R12_quality_normal=c(read.table(paste(R12[1],"/summary.txt",sep=""),sep="\t",stringsAsFactors=F)[,1],
+                       read.table(paste(R12[2],"/summary.txt",sep=""),sep="\t",stringsAsFactors=F)[,1])
 
+  tmp=read.table(paste(R12[1],"/summary.txt",sep=""),sep="\t",stringsAsFactors=F)[,2]
+  names(R12_quality_normal)=c(paste("N1_",tmp,sep=""),paste("N2_",tmp,sep=""))
+  R12_quality_normal=R12_quality_normal[c(paste("N1_",fields,sep=""),paste("N2_",fields,sep=""))]
+  
   # combine
-  quality=rbind(quality,c(coverage,ifelse(c(R12_quality_normal,R12_quality_tumor)<=2,"Pass","Fail")))
+  quality=rbind(quality,c(coverage,R12_quality_normal,R12_quality_tumor))
 }
 
 rownames(quality)=design$sample_id
-colnames(quality)=c("Normal coverage","Tumor coverage","Normal Quality 1",
-  "Normal Quality 2","Tumor Quality 1","Tumor Quality 2")
 write.csv(quality,file=paste(args[2],"/quality.csv",sep=""))
