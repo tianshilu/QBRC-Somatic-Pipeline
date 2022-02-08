ebd3b48f81107b7be856234f2574b195a5835a66
tianshilu <33228841+tianshilu@users.noreply.github.com>
2022-02-08 10:30:56 -0600

    Add files via upload

--- somatic_script/filter_vcf.R
+++ somatic_script/filter_vcf.R
@@ -133,11 +133,20 @@ read_vcf<-function(file)
 {
   cat(paste("Reading",file,"\n"))
   x=tryCatch({
-    read.table(file,stringsAsFactors = F,
-      colClasses=c("character","numeric","character","character","character","character","character",
-                   "character","character","character","character"))
-                   # #CHROM        POS     ID                 REF     ALT       QUAL         FILTER 
-                   #  INFO    FORMAT  normal  tumor
+    if (grepl("lofreq",file))
+    {
+      read.table(file,stringsAsFactors = F,
+                 colClasses=c("character","numeric","character","character","character","character","character",
+                              "character"))
+      # #CHROM        POS     ID                 REF     ALT       QUAL         FILTER INFO    
+    }else
+    {
+      read.table(file,stringsAsFactors = F,
+                 colClasses=c("character","numeric","character","character","character","character","character",
+                              "character","character","character","character"))
+      # #CHROM        POS     ID                 REF     ALT       QUAL         FILTER 
+      #  INFO    FORMAT  normal  tumor
+    }
   }, error = function(e) { # give an empty VCF file
     cat(paste("Warning: failed to read",file,
               ". Maybe this caller didn't find any variants\n"))
